d86aa35852974f9bfe7752aca8ae62102a1bf2e1
Arzivre <sonyfauzi@outlook.co.id>
2022-09-11 19:25:11 +0700

    create layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
 
   return (
-    <h1 className="text-7xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <nav className='mb-6 border-b'>
+        <h1 className='mx-auto max-w-screen-xl py-[18px] px-5 text-[18px] font-bold leading-7'>
+          Product Roadmap
+        </h1>
+      </nav>
+      <main className='mx-auto grid max-w-screen-xl grid-cols-4 gap-4 px-5'>
+       Content Here
+      </main>
+    </>
   )
 }
 
