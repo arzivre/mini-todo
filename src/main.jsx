e3cdd232306d0641ee3c36aa3522aa04f2391855
Arzivre <sonyfauzi@outlook.co.id>
2022-09-11 19:23:56 +0700

    setup router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<Navigate to='/v1' replace />} />
+        <Route path='/v1' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
